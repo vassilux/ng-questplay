4d94f8a9acdd159fe9b4db6a10a368730ec6655f
vassilux <v.gontcharov@gmail.com>
2025-01-19 10:57:47 +0100

    suite ERC20

--- campaigns/token-standards/building-erc-20/contracts/Doubloon.sol
+++ campaigns/token-standards/building-erc-20/contracts/Doubloon.sol
@@ -3,11 +3,65 @@ pragma solidity ^0.8.20;
 
 import "./interfaces/IERC20.sol";
 
-/**
- * @dev ERC-20 token contract.
- */
-contract Doubloon /* is IERC20 */ {
+contract Doubloon {
+    string public constant name = "Doubloon";
+    string public constant symbol = "DBL";
 
-    constructor(uint256 _supply) { }
+    uint256 public totalSupply; 
 
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+
+    
+    constructor(uint256 _supply) {
+        totalSupply = _supply; 
+        balanceOf[msg.sender] = totalSupply; 
+    }
+
+    function transfer(address _to, uint256 _value) external returns (bool) {
+        require(balanceOf[msg.sender] >= _value, "Saldo insufficiente");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+ 
+    function approve(
+        address _spender,
+        uint256 _amount
+    ) external returns (bool) {
+        allowance[msg.sender][_spender] = _amount;
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+
+  
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) external returns (bool) {
+        require(balanceOf[_from] >= _amount, "Balance insufficiente");
+        require(
+            allowance[_from][msg.sender] >= _amount,
+            "Allowance insufficiente"
+        );
+
+        allowance[_from][msg.sender] -= _amount;
+        balanceOf[_from] -= _amount;
+        balanceOf[_to] += _amount;
+        emit Transfer(_from, _to, _amount);
+        return true;
+    }
 }
--- campaigns/token-standards/building-erc-20/contracts/MintableDoubloon.sol
+++ campaigns/token-standards/building-erc-20/contracts/MintableDoubloon.sol
@@ -4,7 +4,21 @@ pragma solidity ^0.8.20;
 import "./Doubloon.sol";
 
 contract MintableDoubloon is Doubloon {
+    address public owner;
 
-    constructor(uint256 _supply) Doubloon(_supply) {}
+    constructor(uint256 _supply) Doubloon(_supply) {
+        owner = msg.sender; 
+    }
 
+    function mint(address _to, uint256 _amount) external {
+        require(
+            msg.sender == owner,
+            "Only owner is allowed"
+        );
+
+        uint256 mintAmount = _amount; 
+        totalSupply += mintAmount; 
+        balanceOf[_to] += mintAmount; 
+        emit Transfer(address(0), _to, mintAmount); 
+    }
 }
